9734b61e20ae6e93f81307504032cc5edac0381b
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2016-04-23 10:18:10 -0500

    Stubs of actions

--- src/components/Bartender.jsx
+++ src/components/Bartender.jsx
@@ -3,13 +3,19 @@ import React from 'react';
 class Bartender extends React.Component {
     constructor(props) {
         super(props);
+        
+        this.pourBeer = this.pourBeer.bind(this);
+    }
+    
+    pourBeer() {
+        console.log('bartender pours the customer more beer');
     }
 
     render() {
         return (
             <div className="well">
                 <h2>Bartender</h2>
-                <button className="btn btn-primary">Pour beer</button>
+                <button className="btn btn-primary" onClick={ this.pourBeer }>Pour beer</button>
             </div>
         );
     }
--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -3,14 +3,25 @@ import React from 'react';
 class Customer extends React.Component {
     constructor(props) {
         super(props);
+        
+        this.askForMore = this.askForMore.bind(this);
+        this.drinkBeer = this.drinkBeer.bind(this);
+    }
+
+    askForMore(){
+        console.log('customer requests more beer');
     }
     
+    drinkBeer() {
+        console.log('customer drinks his or her beer');
+    }
+
     render() {
         return (
             <div className="well">
                 <h2>Customer</h2>
-                <button className="btn btn-primary">Drink beer</button>
-                <button className="btn btn-primary">Ask for more</button>
+                <button className="btn btn-primary" onClick={ this.drinkBeer }>Drink beer</button>
+                <button className="btn btn-primary" onClick={ this.askForMore }>Ask for more</button>
             </div>
         );
     }
