7bb80c07619bc53a2f063c0f3d42f65ac320dfd9
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2016-04-23 10:10:00 -0500

    Better jsx

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 
 import Pub from './components/Pub.jsx';
 
-let theDuck = <Pub name="The Drunken Duck" />
-let theDuke = <Pub name="The Duke Of Argyles" />
+let pubs = (
+    <div>
+        <Pub name="The Drunken Duck" />
+        <Pub name="The Duke Of Argyles" />
+    </div>
+);
 
-ReactDOM.render(<div>{theDuck}{theDuke}</div>, document.getElementById('app')); 
+ReactDOM.render(pubs, document.getElementById('app')); 
