551ef917d70b8f3a6b924b0a56b41b83e466c450
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2016-04-24 18:45:12 -0500

    Simpler

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const Keg = ({ remaining }) => {
-    return (
-        <div className="well">
-            <h2>Keg</h2>
-            <div>Remaining: { remaining }%</div>
-        </div>
-    );
-};
+const Keg = ({ remaining }) => (
+    <div className="well">
+        <h2>Keg</h2>
+        <div>Remaining: { remaining }%</div>
+    </div>
+);
 
 Keg.propTypes = {
     remaining: React.PropTypes.number.isRequired
--- src/components/Mug.js
+++ src/components/Mug.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-const Mug = ({ remaining, beer }) => {
-    return (
-            <div className="well">
-                <h2>Mug</h2>
-                <p>Beer: { beer }</p>
-                <div>Remaining: { remaining }%</div>
-            </div>
-        );
-};
+const Mug = ({ remaining, beer }) => (
+    <div className="well">
+        <h2>Mug</h2>
+        <p>Beer: { beer }</p>
+        <div>Remaining: { remaining }%</div>
+    </div>
+);
 
 Mug.propTypes = { 
     beer: React.PropTypes.string.isRequired,
