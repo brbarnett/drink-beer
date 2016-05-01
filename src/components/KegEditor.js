39174f47720ce6605003655697f2108cad21b556
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2016-05-01 11:38:49 -0500

    Passing in update

--- src/components/KegEditor.js
+++ src/components/KegEditor.js
@@ -4,14 +4,17 @@ const KegEditor = ({ remaining, setKegRemaining }) => {
    return (
         <div className="well">
             <h2>Keg</h2>
-            <div>Remaining: <input /></div>
+            <div>Remaining: 
+                <input type="number" rel="inputString" />
+            </div>
             <button className="btn btn-primary" onClick={ setKegRemaining }>Update</button>
         </div>
     );
 };
 
 KegEditor.propTypes = {
-    remaining: React.PropTypes.number.isRequired
+    remaining: React.PropTypes.number.isRequired,
+    setKegRemaining: React.PropTypes.func.isRequired
 };
 
 export default KegEditor
--- src/containers/KegEditorContainer.js
+++ src/containers/KegEditorContainer.js
@@ -11,7 +11,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return { 
-      poursetKegRemainingBeer: () => {
+      setKegRemaining: () => {
           //dispatch(pourBeer())
           console.log('update');
       } 
